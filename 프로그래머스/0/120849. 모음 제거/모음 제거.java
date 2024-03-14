5eb123a83ce3924e1fe72c6fa67d3eedcec0c8c7
KangJinha <121327609+KangJinha@users.noreply.github.com>
2024-03-14 11:58:10 +0900

    [level 0] Title: 모음 제거, Time: 0.29 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] mo = {"a", "e", "i", "o", "u"};
+        
+        for(int i=0; i<mo.length; i++){
+            if(my_string.contains(mo[i])){
+                answer = my_string.replaceAll(mo[i], "");
+                my_string = answer;
+            } else{
+                answer = my_string;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
